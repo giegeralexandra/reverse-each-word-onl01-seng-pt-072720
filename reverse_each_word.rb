6425d5eb01350f1b7a9f81d3196480341287d0e8
Alexandra Gieger <giegeralexandra@gmail.com>
2020-08-04 02:48:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  new_array = []
-  array.each do |word|
-    new_array << word.reverse
-  end
-  return new_array.join " "
+  array.collect{|x| x.reverse}
+  return array.join " "
 end
